ef7a8252826485ca2fa3dad0d4fed9797b3e7588
Pranjal Pandit <pranjal.pandit@ibibogroup.com>
2016-02-22 14:36:58 +0530

    dirty version but working

--- worktree.go
+++ worktree.go
@@ -5,52 +5,70 @@ import (
 	"time"
 )
 
-type aggStruct struct{
-	index	int
-	data	interface{}
+type AggStruct struct{
+	Index	int
+	Data	interface{}
+	Sentinal	bool
 }
 
 type channelStruct struct{
-	quitChan chan bool
-	size int
-	aggregator chan aggStruct
-	FetchFunctions       []func(inp interface{}) interface{}
-	FetchFunctionsInput  []interface{}
-	Combiner	func(index int, inp interface{}) interface{}
+	size	int
+	aggregator	chan AggStruct
+	parent	chan AggStruct
+	FetchFunctions	[]func(inp interface{}, agg chan AggStruct) interface{}
+	FetchFunctionsInput	[]interface{}
+	Combiner	func(index int, inp interface{}, allR interface{}) (interface{}, bool)
 }
 
-func (cS *channelStruct) AddFetcher (f func(inp interface{}) interface{}, input interface{}) {
+/*func NewChannelStruct() chan AggStruct {
+	cS := channelStruct{}
+	cS.aggregator = make(chan AggStruct, cS.size)
+	cS.quitChan = make(chan bool, cS.size)
+	return cS.aggregator
+}*/
+
+func (cS *channelStruct) AddFetcher (f func(inp interface{}, agg chan AggStruct) interface{}, input interface{}) {
 	cS.FetchFunctions = append(cS.FetchFunctions, f)
 	cS.FetchFunctionsInput = append(cS.FetchFunctionsInput, input)
 	cS.size = cS.size + 1
 }
 
-func (cS *channelStruct) AddCombiner (f func(index int, inp interface{}) interface{}) {
+func (cS *channelStruct) AddCombiner (f func(index int, inp interface{}, allR interface{}) (interface{}, bool)) {
 	cS.Combiner = f
 }
 
-func (cS *channelStruct) Do (f func(inp []interface{}) interface{}) {
+func retNil() interface{} {
+	return nil
+}
+
+func (cS *channelStruct) Do ()  {
 	//I keep the channels in this slice, and want to "loop" over them in the select statement
-
-	cS.aggregator = make(chan aggStruct, cS.size)
-	cS.quitChan = make(chan bool, cS.size)
-
 	for index, function := range cS.FetchFunctions {
 		go cS.workAndQuit(index, function, cS.FetchFunctionsInput[index])
 	}
 
+	cS.aggregator = make(chan AggStruct, cS.size)
+	cS.parent = make(chan AggStruct, cS.size)
+
 	breakFlag := false
 	received := 0
+	resp := retNil()
+	sendToParent := false
 	for received < cS.size {
 		select {
 			case msg := <-cS.aggregator:
-				fmt.Println("received ", msg.data)
-				fmt.Println("index ", msg.index)
-				cS.Combiner(msg.index, msg.data)
-			case <- cS.quitChan:
-				received = received + 1
+				if msg.Sentinal == true{
+					received = received + 1
+					break
+				}
+				resp, sendToParent = cS.Combiner(msg.Index, msg.Data, resp)
+				if sendToParent == true{
+					agg := AggStruct{msg.Index, resp, false}
+					cS.parent <- agg
+				}
+				fmt.Println("===resp=", resp)
+				// cs.Parent <- resp ***
 			case <-time.After(2 * time.Second):
-				fmt.Println("timimg out")
 				breakFlag = true
 				break
 		}
@@ -60,21 +78,44 @@ func (cS *channelStruct) Do (f func(inp []interface{}) interface{}) {
 	}
 }
 
-func (cS *channelStruct) work(index int, function func(inp interface{}) interface{}, payload interface{}) {
+func (cS *channelStruct) work(index int, function func(inp interface{}, agg chan AggStruct) interface{}, payload interface{}) {
 	//time.Sleep(3*time.Second)
-	cS.aggregator <- aggStruct{index, function(payload)}
+	// If you create a new work tree in child mapper; function(payload); 
+	// assign cs.parent=that channel and send resp to cs.Parent ***
+	fOut := function(payload, cS.aggregator)
+	agg := AggStruct{index, fOut, false}
+	cS.aggregator <- agg
 }
 
-func (cS *channelStruct) workAndQuit(index int, function func(inp interface{}) interface{}, payload interface{}){
+func (cS *channelStruct) workAndQuit(index int, function func(inp interface{}, agg chan AggStruct) interface{}, payload interface{}){
 	// wrapped user defined function
 	cS.work(index, function, payload)
-	cS.quitChan <- true
+	agg := AggStruct{0, nil, true}
+	cS.aggregator <- agg
+}
+
+func fetcher(inp interface{}, _ chan AggStruct) interface{} {
+	i := inp.(int)
+	return i
+}
+
+func combiner(index int, data interface{}, resp interface{}) (interface{}, bool) {
+	out := 0
+	if resp != nil{
+		out = data.(int) + resp.(int)
+	}else{
+		out = data.(int)
+	}
+	return out, false
 }
 
 func main() {
-
+	//ch := make (chan AggStruct)
 	cS := channelStruct{}
-	cS.AddFetcher()
+	cS.AddFetcher(fetcher, 2)
+	cS.AddFetcher(fetcher, 3)
+	cS.AddCombiner(combiner)
+	cS.Do()
 
 	/*for i := 0; i < numChans; i++ {
 		msg := <-agg
